bc7362ab8e0ff1e213d79fb351f4b9434cc72005
JPMMaia <jpmmaia@gmail.com>
2026-04-17 22:52:58 +0300

    Do not overwrite VCPKG_ROOT when running Enter-VsDevEnv

--- Scripts/Enter-VsDevEnv.ps1
+++ Scripts/Enter-VsDevEnv.ps1
@@ -1,9 +1,16 @@
 function Enter-VsDevEnv {
+    $originalVcpkgRoot = $env:VCPKG_ROOT
     $vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
     cmd /c "`"$vsPath\Common7\Tools\VsDevCmd.bat`" -arch=x64 -host_arch=x64 && set" |
     ForEach-Object {
         if ($_ -match '^(.*?)=(.*)$') {
             Set-Item -Path "env:$($matches[1])" -Value $matches[2]
         }
+    }
+
+    if ($null -ne $originalVcpkgRoot) {
+        Set-Item -Path "env:VCPKG_ROOT" -Value $originalVcpkgRoot
+    } else {
+        Remove-Item -Path "env:VCPKG_ROOT" -ErrorAction SilentlyContinue
     }
 }
