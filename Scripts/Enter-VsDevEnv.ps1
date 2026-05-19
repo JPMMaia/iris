5703e4a9d3132a7b664db454089675735cb8e96f
JPMMaia <jpmmaia@gmail.com>
2026-05-19 18:51:50 +0300

    Improve Enter-VsDevEnv script

--- Scripts/Enter-VsDevEnv.ps1
+++ Scripts/Enter-VsDevEnv.ps1
@@ -1,7 +1,7 @@
 function Enter-VsDevEnv {
     $originalVcpkgRoot = $env:VCPKG_ROOT
-    $vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
-    cmd /c "`"$vsPath\Common7\Tools\VsDevCmd.bat`" -arch=x64 -host_arch=x64 && set" |
+    $vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -all -products * -latest -property installationPath
+    cmd /c "`"$vsPath\Common7\Tools\VsDevCmd.bat`" -arch=x64 -host_arch=x64 `-vcvars_ver=14.42.34433 && set" |
     ForEach-Object {
         if ($_ -match '^(.*?)=(.*)$') {
             Set-Item -Path "env:$($matches[1])" -Value $matches[2]
