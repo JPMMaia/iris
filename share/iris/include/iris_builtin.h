4d982c03a8eeaac8c117c3eccc3409b2a79d81f7
JPMMaia <jpmmaia@gmail.com>
2026-06-06 12:12:12 +0300

    Export Decimals in C headers

--- share/iris/include/iris_builtin.h
+++ share/iris/include/iris_builtin.h
@@ -12,4 +12,23 @@ struct Array_slice_Uint16{ uint16_t* data; uint64_t size; };
 struct Array_slice_Uint32{ uint32_t* data; uint64_t size; };
 struct Array_slice_Uint64{ uint64_t* data; uint64_t size; };
 
+typedef int32_t Decimal1;
+typedef int32_t Decimal2;
+typedef int32_t Decimal3;
+typedef int32_t Decimal4;
+typedef int32_t Decimal5;
+typedef int32_t Decimal6;
+typedef int64_t Decimal7;
+typedef int64_t Decimal8;
+typedef int64_t Decimal9;
+typedef int64_t Decimal10;
+typedef int64_t Decimal11;
+typedef int64_t Decimal12;
+typedef int64_t Decimal13;
+typedef int64_t Decimal14;
+typedef int64_t Decimal15;
+typedef int64_t Decimal16;
+typedef int64_t Decimal17;
+typedef int64_t Decimal18;
+
 #endif
